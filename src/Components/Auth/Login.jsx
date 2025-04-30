e9d426641305544aa7afcbcb639b27b24b86e73e
shohag390 <shohag390@users.noreply.github.com>
2025-04-30 13:11:33 +0600

    Google functunality Add

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../../Firebase/firebase.init';
+
 
 const Login = () => {
+    const [user, setUser] = useState(null);
+
+    const provider = new GoogleAuthProvider
+
+
+    const handleGoogleSignIn = () => {
+
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result?.user);
+
+                setUser(result?.user)
+            })
+            .catch((error) => {
+                console.log(error);
+
+            })
+
+    }
+
     return (
         <div className='h-[90vh] bg-amber-900 text-white'>
-            <h1>Login</h1>
+            <h1>Please Login</h1>
+
+            <button onClick={handleGoogleSignIn} className='border-[2px] border-white py-[10px] px-[30px] cursor-pointer'>Sign in with google</button>
+
+            <div>
+                {
+                    user && (
+                        <div>
+                            <h3>{user.displayName}</h3>
+                            <h3>{user.email}</h3>
+                            <img src={user?.photoURL} alt="image" />
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -16,4 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
+
